c84743140b35d43e67a4d38703ed7578387cabb7
shenyuanchen <shenyuanchen@mail.xusenintl.com>
2017-02-24 18:15:27 +0800

    'orangi'

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,4 @@
 @import '~vux/src/styles/reset.less';
 @import 'assets/style/reset.scss';
 @import 'assets/font-awesome-4.7.0/css/font-awesome.min.css';
-
-  body {
-    background-color: #fbf9fe;
-  }
-  body, html {
-    height: 100%;
-    width: 100%;
-    overflow-x: hidden;
-  }
 </style>
--- src/components/OrganizationStructure/OrganizationStructure.com.vue
+++ src/components/OrganizationStructure/OrganizationStructure.com.vue
@@ -1,14 +1,83 @@
 <template>
   <div class="full-page">
-    <h1>組織架構</h1>
+    <flexbox class="person-ui" orient="vertical" direction="column" :gutter="0">
+        <div class="person-department">
+        <ul class="department-list">
+          <li class="active">
+            <a href="javascript:;">
+              <i class="fa fa-chevron-left ver-m"></i>
+              <span class="ver-m">集团一</span>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <i class="fa fa-chevron-left ver-m"></i>
+              <span class="ver-m">部门一</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <flexbox-item>
+        <div class="person-list-content">
+          <h6 class="person-assign">下级部门</h6>
+          <div class="person-list">
+            <dl>
+              <dd class="list-item">
+                <flexbox>
+                  <flexbox-item>
+                    <div class="ellipsis">
+                      AA
+                    </div>
+                  </flexbox-item>
+                  <div class="available-to">
+                    <span class="countable">169</span>
+                    <i class="fa fa-chevron-right"></i>
+                  </div>
+                </flexbox>
+              </dd>
+              <dd class="list-item">
+                <flexbox>
+                  <flexbox-item>
+                    <div class="ellipsis">
+                      CC
+                    </div>
+                  </flexbox-item>
+                  <div class="available-to">
+                    <span class="countable">12</span>
+                    <i class="fa fa-chevron-right"></i>
+                  </div>
+                </flexbox>
+              </dd>
+              <dd class="list-item">
+                <flexbox>
+                  <flexbox-item>
+                    <div class="ellipsis">
+                      DD
+                    </div>
+                  </flexbox-item>
+                  <div class="available-to">
+                    <span class="countable">42</span>
+                    <i class="fa fa-chevron-right"></i>
+                  </div>
+                </flexbox>
+              </dd>
+            </dl>
+          </div>
+        </div>
+      </flexbox-item>
+    </flexbox>
   </div>
 </template>
-<style></style>
+<style>
+  @import 'css/organization.scss';
+</style>
 <script>
-  import { ViewBox } from 'vux'
+  import { ViewBox, Flexbox, FlexboxItem } from 'vux'
   export default{
     components: {
-      ViewBox
+      ViewBox,
+      Flexbox,
+      FlexboxItem
     }
   }
 </script>
